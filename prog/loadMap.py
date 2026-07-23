d5a35a8dee0197dba1efb082fe08311ec4d2b9c4
VladimirKataev <vova.kataev@gmail.com>
2026-07-22 19:31:38 -0700

    added a test file, now can process

--- prog/loadMap.py
+++ prog/loadMap.py
@@ -1,12 +1,15 @@
-import pandas as pd 
 import csv
 
 """
     we need the following:
-        pathsTouching = {fromGlobalId, toGlobalId, altitude, name, direction}
+        paths = [{fromGlobalId, toGlobalId, altitude, name, direction}]
             - NOTE: a->b is different from b->a. Odd people fly East.
         neighbors[globalID] = pathsTouching[]
+        getPointID[VRP] = globalID
 """
 
-def loadPoints(filename : str):
-    ans = {}
+def loadData(filename : str) -> tuple[dict, dict, dict]:
+    with open(filename, mode='r', newline='', encoding='utf-8') as file:
+        reader = csv.reader(file);
+        for row in reader:
+            print(row)
